7d51e4620191f12a0373f070114c04b0b90dbf4c
samsmit-reul <s.ahimbisibwe@students.vu.ac.ug>
2023-03-21 16:05:53 +0300

    added some lines of header code

---
+++ includes/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation bar</title>
+    <link rel="stylesheet" href="../css/style.css" media="all">
+</head>
+<body>
+<nav class="list">
+    <div class="navbar-items1">
+    <div class="icon">
+        <img src="images/logo.png"  width="50" height="50"alt="">
+    </div>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Products</a></li> 
+    <li><a href="#">Contact</a></li>
+   </ul>
+</div>
+<div class="navbar-items2">
+   <div class="searchbar">
+   <form>
+        <input type="text" placeholder="Search">
+        <button type="search">Search</button>
+      </form>
+   </div>
+   <div class="cart">
+    <img src="../images/cart.png" height="50" width="50" alt="">
+   </div>
+   <div class="usericon">
+    <img src="../images/user.png" width="50" height="50" alt="">
+   </div>
+</div>
+</nav> 
+</body>
+</html>
