f1c51f5ac63e901883fcc771f6afdefbb81b8738
samsmit-reul <s.ahimbisibwe@students.vu.ac.ug>
2023-03-23 14:40:54 +0300

    adding some files

---
+++ admin_login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Admin Login</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    
+  <div class="login-box">
+    <h2>Admin Login</h2>
+    <form action="includes/sidebar.php" method="post">
+      <label for="username">Username</label>
+      <input type="text" name="username" required>
+      <label for="password">Password</label>
+      <input type="password" name="password" required>
+      <input type="submit" name="submit" value="Login" >
+    </form>
+  </div>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>sidebar</title>
+</head>
+<body>
+<nav class="navbar">
+  <div class="navbar-logo">
+    <img src="images/logo.png" width="50" height="50" alt="">
+  </div>
+  <ul class="navbar-menu">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Products</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+  <form action="#" class="navbar-search">
+    <input type="text" placeholder="Search...">
+    <button type="submit">Search</button>
+  </form>
+  <div class="cart">
+    <img src="images/cart.png" width="50" height="50" alt="">
+  </div>
+  <div class="usericon">
+    <img src="images/user.png" width="50" height="50" alt="">
+  </div>
+</nav>
+</body>
+</html>
